3eec3d102c6d18e4c716303cdc6537382e72d939
Nico Rithner <nicorithner@gmail.com>
2023-04-12 17:19:50 -0600

    Create packageNetworks seeder

---
+++ app/db/seeders/20230412213046-seed-packageNetworks.js
@@ -0,0 +1,70 @@
+const db = require("../models");
+const Network = db.Network;
+const Package = db.Package;
+const packagesJSON = require("./data/packages.json");
+
+async function getPackageNetworks() {
+  const packageNetworksObjects = [];
+  const networkIds = {};
+  const packageIds = {};
+  const packageAndNetworks = [];
+
+  // -- Create NetworkIds dictionary
+
+  const networks = await Network.findAll();
+
+  for (const network of networks) {
+    networkIds[network.name] = network.id;
+  }
+
+  // -- Create PackageIds dictionary
+
+  const packages = await Package.findAll();
+
+  for (const package of packages) {
+    packageIds[package.name] = package.id;
+  }
+
+  // get networks in a package
+  // for (const package of packagesJSON) {
+  //   packageAndNetworks[package.name] = {
+  //     id: packageIds[package.name],
+  //     networks: package.networks.map((net) => networkIds[net]),
+  //   };
+  // }
+  for (const package of packagesJSON) {
+    packageAndNetworks.push(
+      ...package.networks.map((net) => [
+        networkIds[net],
+        packageIds[package.name],
+      ])
+    );
+  }
+
+  // ---- Create packageNetworks
+
+  for (const pair of packageAndNetworks) {
+    packageNetworksObjects.push({
+      NetworkId: pair[0],
+      PackageId: pair[1],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+  return packageNetworksObjects;
+}
+getPackageNetworks();
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, _Sequelize) {
+    await queryInterface.bulkInsert(
+      "PackageNetworks",
+      await getPackageNetworks(),
+      {}
+    );
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete("PackageNetworks", null, {});
+  },
+};

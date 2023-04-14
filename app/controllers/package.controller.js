1838bf18203175527c14dc862f90ca9d9a3ff199
Nico Rithner <nicorithner@gmail.com>
2023-04-14 10:58:49 -0600

    Switch package controller to use try catch

--- app/controllers/package.controller.js
+++ app/controllers/package.controller.js
@@ -1,10 +1,10 @@
 const db = require("../db/models");
 const Package = db.package;
 const Network = db.network;
-const Op = db.Sequelize.Op;
+
 // Create and Save a new Package
-exports.create = (req, res) => {
-  if (!req.body.name) {
+exports.create = async (req, res) => {
+  if (!req.body.name || !req.body.price) {
     res.status(400).send({
       message: "Content can not be empty",
     });
@@ -16,34 +16,31 @@ exports.create = (req, res) => {
     price: req.body.price,
   };
 
-  Package.create(package)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          `Something went wrong while creating package ${data.name}`,
-      });
+  try {
+    const newPackage = await Package.create(package);
+    res.send(newPackage);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        `Something went wrong while creating package ${package.name}`,
     });
+  }
 };
 
 // Retrieve all Packages from the database.
-exports.findAll = (req, res) => {
-  Package.findAll({
-    where: {},
-    truncate: false,
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Something went wrong while retrieving packages",
-      });
+exports.findAll = async (req, res) => {
+  try {
+    const packages = await Package.findAll({
+      where: {},
+      truncate: false,
     });
+    res.send(packages);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Something went wrong while retrieving packages",
+    });
+  }
 };
 
 // Find a single Package with an id. Includes networks
@@ -63,76 +60,79 @@ exports.findOne = async (req, res) => {
     res.send(package);
   } catch (err) {
     res.status(500).send({
-      message: err.message || `Something went wrong while retrieving package with id=${id}`,
+      message:
+        err.message ||
+        `Something went wrong while retrieving package with id=${id}`,
     });
   }
 };
 
 // Update a Package by the id in the request
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
 
-  Package.update(req.body, {
+  const num = await Package.update(req.body, {
     where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: "Package was updated successfully.",
-        });
-      } else {
-        res.send({
-          message: `Couldn't update package with id=${id}`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          `Something went wrong while updating package with id=${id}`,
-      });
+  });
+
+  try {
+    Package.update(req.body, {
+      where: { id: id },
     });
+    if (num == 1) {
+      res.send({
+        message: "Package was updated successfully.",
+      });
+    } else {
+      res.send({
+        message: `Couldn't update package with id=${id}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        `Something went wrong while updating package with id=${id}`,
+    });
+  }
 };
 
 // Delete a Package with the specified id in the request
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
 
-  Package.destroy({
+  const num = await Package.destroy({
     where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: "Package was deleted successfully!",
-        });
-      } else {
-        res.send({
-          message: `Couldn't delete package with id=${id}`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || `Couldn't detele package with id=${id}`,
+  });
+  try {
+    if (num == 1) {
+      res.send({
+        message: "Package was deleted successfully!",
       });
+    } else {
+      res.send({
+        message: `Couldn't delete package with id=${id}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || `Couldn't detele package with id=${id}`,
     });
+  }
 };
 
 // Delete all Packages from the database.
-exports.deleteAll = (req, res) => {
-  Package.destroy({
-    where: {},
-    truncate: false,
-  })
-    .then((nums) => {
-      res.send({ message: `${nums} Packages were deleted successfully!` });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all packages.",
-      });
+exports.deleteAll = async (req, res) => {
+  try {
+    const nums = await Package.destroy({
+      where: {},
+      truncate: false,
     });
+    res.send({ message: `${nums} Packages were deleted successfully!` });
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while removing all packages.",
+    });
+  }
 };

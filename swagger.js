4b862385664eda6957a48c70950b7ccd0b2b8b93
Nico Rithner <nicorithner@gmail.com>
2023-05-26 15:47:10 -0600

    Update title

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Charter-Spectrum Entertainment API",
+      title: "Entertainment Packages API 2023",
       version: "1.0.0",
       description:
         "This API serves endpoints to entertainment packages with their networks and shows",
